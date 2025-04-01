dd9f06d7e15ba1604ab62a85d8adff0fa862affb
Geo <gjsj@discente.ifpe.edu.br>
2025-04-01 16:56:19 -0300

    main project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from "./components/menu/menu.component";
 import{ ProfileImgComponent }from "./components/profile-img/profile-img.component"
+import{ FooterComponent }from "./components/footer/footer.component"
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuComponent, ProfileImgComponent],
+  imports: [RouterOutlet, MenuComponent, ProfileImgComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/botton-cv/botton-cv.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-botton-cv',
+  standalone: true,
+  imports: [],
+  templateUrl: './botton-cv.component.html',
+  styleUrl: './botton-cv.component.css'
+})
+export class BottonCvComponent {
+
+}
